abbe25f3a8f3ba6b5e8714f49776ec551e14324e
xerxis alvar <xerxis@flok.co>
2014-10-08 12:18:36 +0800

    Setup that works on Windows 7

--- Vagrantfile
+++ Vagrantfile
@@ -5,42 +5,45 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
- 
-  # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "ubuntu/trusty64"
+  
+   # Vagrant box to use  Ubuntu
+   config.vm.box = "ubuntu/trusty64"
 
-  # Disable automatic box update checking. If you disable this, then
-  # boxes will only be checked for updates when the user runs
-  # `vagrant box outdated`. This is not recommended.
-  # config.vm.box_check_update = false
-
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine. In the example below,
-  # accessing "localhost:8080" will access port 80 on the guest machine.
-    config.vm.network "forwarded_port", guest: 80, host: 80
-    config.vm.network "forwarded_port", guest: 3306, host:3306
-
-  # Provisioner  
-    config.vm.provision :shell, :path => "provision.sh"
+   # Hostname
+   config.vm.hostname = "nix"
+   
+   
+  # Forward port to host
+  config.vm.network "forwarded_port", guest: 80,    host: 80
+  config.vm.network "forwarded_port", guest: 3306,  host: 3306
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  config.vm.network "private_network", ip: "192.168.11.11"
-
-  # Create a public network, which generally matched to bridged network.
-  # Bridged networks make the machine appear as another physical device on
-  # your network.
-  # config.vm.network "public_network"
-
-  # If true, then any SSH connections made will enable agent forwarding.
-  # Default value: false
-  # config.ssh.forward_agent = true
-
+  config.vm.network "private_network", ip: "10.10.10.10"
+  
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  config.vm.synced_folder "www", "/var/www"
-
+  config.vm.synced_folder "www", "/var/www/"
   
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # Example for VirtualBox:
+  #
+  config.vm.provider "virtualbox" do |v|
+    # Don't boot with headless mode
+    v.gui = false
+
+    # Use VBoxManage to customize the VM. For example to change memory:
+    v.customize ["modifyvm", :id, "--memory",               "512"]
+    v.customize ["modifyvm", :id, "--cpuexecutioncap",      "95"]
+    v.customize ["modifyvm", :id, "--natdnshostresolver1",  "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1",         "on"]
+  end
+
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
+
+  # Custom shell provisioner
+  config.vm.provision :shell, path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,16 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
 
-bash -c 'BASH_ENV=/etc/profile exec bash'
+sudo aptitude update -q
 
-echo "Provisioning virtual machine..."
+# Force a blank root password for mysql
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
 
+# Install mysql, nginx, php5-fpm
+sudo aptitude install -q -y -f mysql-server mysql-client nginx php5-fpm
 
-# Nginx and Git
-echo "Installing Nginx......."
-apt-get install nginx git -y -q  --fix-missing
+# Install commonly used php packages
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-mcrypt
 
+# Install and configure PhpMyAdmin
+sudo aptitude install -q -y -f phpmyadmin
 
-# Configure NGINX
+#create /var/www
+sudo mkdir /var/www
+
+# Configure Nginx
 sudo rm /etc/nginx/sites-available/default
 sudo touch /etc/nginx/sites-available/default
 
@@ -28,9 +37,7 @@ server {
     try_files $uri $uri/ /index.php$is_args$args;
   }
 
-
   # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
-  #
   location ~ \.php$ {
     try_files $uri =404;
     fastcgi_split_path_info ^(.+\.php)(/.+)$;
@@ -45,64 +52,45 @@ server {
   location ~ /\.ht {
     deny all;
   }
+  
+   ### phpMyAdmin ###
+  location /phpmyadmin {
+    root /usr/share/;
+    index index.php index.html index.htm;
+    location ~ ^/phpmyadmin/(.+\.php)$ {
+      client_max_body_size 4M;
+      client_body_buffer_size 128k;
+      try_files $uri =404;
+      root /usr/share/;
 
- 
-    
+      # Point it to the fpm socket;
+      fastcgi_pass unix:/var/run/php5-fpm.sock;
+      fastcgi_index index.php;
+      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+      include /etc/nginx/fastcgi_params;
+    }
+
+    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
+      root /usr/share/;
+    }
+  }
+  location /phpMyAdmin {
+    rewrite ^/* /phpmyadmin last;
+  }
+  ### phpMyAdmin ###
+
 }
 EOF
 
-
-
-
-
-# PHP
-echo "Updating PHP repository......."
-apt-get install python-software-properties build-essential -y -q
-add-apt-repository ppa:ondrej/php5 -y -q
-apt-get update -y -q
-
-echo "Installing PHP........."
-apt-get install php5-common php5-dev php5-cli php5-fpm -y -q
-
-echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y
-
-# MySQL 
-echo "Install and configure MySQL .......... "
-
-apt-get install debconf-utils -y -q
-
-debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-
-apt-get install mysql-server -y -q
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-
+# Allow host to access MySQL server
+sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 
+# Activate Mcrypt
+sudo php5enmod mcrypt
 
-# Configure php5-fpm
-echo "Configure php5-fpm  ............... "
-sed -i s/\;cgi\.fix_pathinfo\s*\=\s*1/cgi.fix_pathinfo\=0/ /etc/php5/fpm/php.ini
-
-
-# Install phpmyadmin and configure
-echo "Install and configure PhpMyAdmin"
-sudo apt-get install -y phpmyadmin
-
-ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
-
-php5enmod mcrypt
-
-
-# Restart services
-echo "Restart services..."
-
-sudo service php5-fpm restart
-
+# Restart Services
+sudo service mysql restart
 sudo service nginx restart
-
-sudo /etc/init.d/mysql restart
-
-clear
+sudo service php5-fpm restart
