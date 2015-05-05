bd295370af18f088f7a04a4fb2e1ea886b3429cd
xerxis <kaixersoft@gmail.com>
2015-05-05 14:55:01 +0800

    Update provision.sh
    Fix : Not able to continue composer update "Cannot allocate memmory"

--- provision.sh
+++ provision.sh
@@ -98,7 +98,10 @@ sudo mv composer.phar /usr/local/bin/composer
 sudo chmod +x /usr/local/bin/composer
 
 
-
+# Fix  Not able to continue composer update
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
 
 
 
